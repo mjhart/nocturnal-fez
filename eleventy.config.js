f8d808daa7833ede8b964fb533829400d79080c9
Matthew Hart <matthart4@gmail.com>
2026-03-08 19:51:17 -0400

    add logo

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,7 @@
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+
 export default function(eleventyConfig) {
   eleventyConfig.ignores.add("README.md");
   eleventyConfig.addPassthroughCopy("bundle.css");
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin);
 };
